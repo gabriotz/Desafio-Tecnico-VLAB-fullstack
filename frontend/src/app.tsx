c9901230345b77aca3f38f6a60533530041b29eb
gabriotz <gmomenezes@gmail.com>
2026-03-02 00:10:02 -0300

    (feature/ResourceList): Clean app.tsx

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,16 +1,6 @@
-import { useState } from "react"
-import { ResourceModal } from "@/components/ResourceModal"
-
 function App() {
-  const [open, setOpen] = useState(true) // true pra já abrir
-
   return (
     <div>
-      <ResourceModal
-        isOpen={open}
-        onClose={() => setOpen(false)}
-        onSubmit={(data) => console.log(data)}
-      />
     </div>
   )
 }
