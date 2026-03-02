c25ce98c651f286a836c72920c9becb8d7ef4a65
Gabriel Machado <gmomenezes@gmail.com>
2026-03-02 17:45:05 -0300

    (feature/ResourceSearch): Change route prefixx

--- backend/main.py
+++ backend/main.py
@@ -27,7 +27,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(resources_router, prefix="/api/v1")
+app.include_router(resources_router, prefix="/api")
 
 
 @app.get("/health")
